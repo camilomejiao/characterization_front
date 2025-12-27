6c039c7bae09b7de12c8ad60e4efa49c83733f0a
cmejia <camiloa.mejiao@gmail.com>
2025-12-27 01:51:53 -0500

    add new validations to delimiter

--- src/helpers/Global.jsx
+++ src/helpers/Global.jsx
@@ -1,4 +1,4 @@
 export const Global = {
-    url: "http://localhost:3800/characterization/api/v1/"
-    //url: process.env.REACT_APP_API_URL || "/characterization/api/v1/",
+    //url: "http://localhost:3800/characterization/api/v1/"
+    url: process.env.REACT_APP_API_URL || "/characterization/api/v1/",
 };
