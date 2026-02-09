a8f7ad9145aeeea64641c7ff06462b84727b2132
cmejia <camiloa.mejiao@gmail.com>
2026-02-08 21:18:23 -0500

    add new styles

--- src/helpers/Global.jsx
+++ src/helpers/Global.jsx
@@ -1,4 +1,4 @@
 export const Global = {
-    url: process.env.REACT_APP_API_URL || "http://localhost:3800/characterization/api/v1/",
-    //"/characterization/api/v1/",
+    //url: "http://localhost:3800/characterization/api/v1/"
+    url: process.env.REACT_APP_API_URL || "/characterization/api/v1/",
 };
